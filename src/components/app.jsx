83741fa19eb7bc6ae4bf06c90a0d100dca7541bb
Jeroen Haisma <J_Haisma@hotmail.com>
2019-05-24 15:21:11 -0300

    2 screen halfs done

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+// app.jsx
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene"></div>
+        <div className="right-scene"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
