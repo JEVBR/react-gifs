33d27a53b4f2e4dfaf0460e94084f440c0b584c7
Jeroen Haisma <J_Haisma@hotmail.com>
2019-05-27 10:16:51 -0300

    destructured giflist

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -2,18 +2,12 @@
 import React, { Component } from 'react';
 import Gif from './gif.jsx';
 
-class GifList extends Component {
-  renderList = () => {
-    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} selectGif={this.props.selectGif}/>);
-  }
-
-  render() {
-    return (
-      <div className="gif_list">
-        {this.renderList()}
-      </div>
-    );
-  }
-}
+const GifList = ({ gifs, selectGif }) => {
+  return (
+    <div className="gif-list">
+      {gifs.map(({ id }) => <Gif id={id} key={id} selectGif={selectGif} />)}
+    </div>
+  );
+};
 
 export default GifList;
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,14 +2,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ""
+    };
+  }
+
   handleUpdate = (event) => {
     console.log(event.target.value);
     this.props.searchfunction(event.target.value);
+    this.setState({
+      term: event.target.value
+    })
   }
 
   render() {
     return (
       <input
+        value = {this.state.term}
         type="text"
         className="form-control form-search"
         onChange={this.handleUpdate}
